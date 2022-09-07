f8e19343277406cbc97b6515150f98fd7e25c5ec
Koffiebaard <tim@koffiemok.net>
2022-09-07 17:17:58 +0200

    The future is now

---
+++ README.md
@@ -0,0 +1,26 @@
+# Powerpoint to Node.js Compiler
+Write code in your favorite Powerpoint editor and compile very easily into proper Node.js. That's right, the future is now.
+
+![Compile Powerpoint to Node.js](docs/img/powerpoint_compiler.png)
+
+## Getting started
+There are two steps:
+
+1) Write code in Powerpoint
+2) Compile and run as Node.js
+
+That's it. Call the bash script with one argument, the pptx filename, to compile and run your Powerpoint slides. I should mention it's only been tested on Mac.
+
+## Example
+```bash
+$ ./compiler.sh code.pptx
+Filename: code.pptx
+Slide count: 1
+
+- Parsing slide 1 / 1
+
+------------------------
+
+I am testing if this works. 
+I hope so.
+```
---
+++ compiler.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+verbose=1
+
+# Validate filename parameter before proceeding
+if [[ ! -n $1 ]] || [[ ! -f "$1" ]]; then
+  echo "Error. Provide a valid pptx filename to compile.";
+  exit 1;
+fi
+
+filename="$1"
+slide_amount=$(unzip -l "$filename" ppt/slides/slide*.xml | awk '{print $2}' | tail -1);
+code=""
+
+[ $verbose -eq 1 ] && printf "Filename: $filename\nSlide count: $slide_amount\n\n"
+
+# Parse each slide and gather all that delicious code
+for slide_number in `seq 1 $slide_amount`; do
+
+  [ $verbose -eq 1 ] && echo "- Parsing slide $slide_number / $slide_amount"
+
+  code_on_slide=$(unzip -qc "$filename" ppt/slides/slide$slide_number.xml | perl -e 'while(<>) {  if (@list = ($_ =~ m/\<a:t\>(.+?)\<\/a:t\>/g)) { print "$_\n" for @list } }' | tr -d "\n");
+  code="$code $code_on_slide";
+done
+
+[ $verbose -eq 1 ] && printf "\n------------------------\n\n"
+
+# Execute all code we find. What's the first that could happen?
+node -e "$code"
